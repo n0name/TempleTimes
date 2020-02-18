475f4cc7d86ee267fdedd1c3fb0a3c916ad71be9
Nikola Georgiev <georgiev.nikito@gmail.com>
2020-02-18 23:02:57 +0200

    Roomscale test and Portal STUB
    Signed-off-by: Nikola Georgiev <georgiev.nikito@gmail.com>

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Portal Other;
+    public Transform ExitPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player Enter");
+            other.transform.position = Other.ExitPoint.position;
+            other.transform.Rotate(new Vector3(0f, 180f, 0f));
+        }
+    }
+}
